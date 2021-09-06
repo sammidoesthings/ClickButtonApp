1589df448680ac82cc5dc3575213ca99f1355be3
Samantha <samanthaltl.sfh@gmail.com>
2021-09-05 21:42:49 -0700

    added textarea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+const [count, setCount] = useState(0);
+const [paragraph, setParagraph] = useState("empty");
+
+function handleChange(event){
+  console.log(event.target.value)
+  setParagraph(event.target.value)
+}
+
+const increase = () => {
+  let currentValue = count;
+  let newValue = currentValue + 1
+  setCount(newValue)
+  console.log(newValue)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appContainer">
+        <button onClick={increase}>Click me!</button>
+        <button onClick={() => {
+          setCount(0)
+        }}>Reset me!</button>
+            {count}
+            clicks
+            <textarea value={paragraph} onChange={handleChange}></textarea>
     </div>
   );
 }
